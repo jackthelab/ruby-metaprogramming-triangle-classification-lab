d8a79eb15fc5460f6f8682a84c0ebaec1aa83f27
Jacob Hampton <jhampton792@gmail.com>
2021-01-21 16:31:29 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+
+  @@all = []
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if [self.a, self.b, self.c].include?(0)
+      raise TriangleError
+    elsif self.a + self.b <= self.c || self.a + self.c <= self.b || self.b + self.c <= self.a
+      raise TriangleError
+    elsif self.a == self.b && self.a == self.c
+      return :equilateral
+    elsif self.a == self.b || self.a == self.c || self.b == self.c
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    #
+  end
+
+  def self.all
+    @@all
+  end
+
 end
